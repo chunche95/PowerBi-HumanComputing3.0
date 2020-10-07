9ef9a3fa78651730b5c4efe7e8523937de952723
chunche95 <paeste95.pb@gmail.com>
2020-10-07 08:40:11 +0200

    Subida auto de archivos - runtime

---
+++ up.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git add Adjuntos/.
+git nota "Archivos adjuntos del proyecto SIMPLE COMPETITION"
+git sube
+
+git add Imágenes/.
+git nota "Imágenes de archivo"
+git sube
+
+git add *.json
+git nota "Informe de rendimiento de archivo PBIX"
+git sube
+
+git add *.pbix
+git nota "Reportes"
+git sube
+
+git add *.rar
+git nota "Documentación de ayuda"
+git sube
+
+git add *.html
+git nota "Resumen de documentos"
+git sube
+
+git add README.md
+git nota "Proyecto PowerBi"
+git sube
+
+git add up.sh
+git nota "Subida auto de archivos - runtime"
+git sube
